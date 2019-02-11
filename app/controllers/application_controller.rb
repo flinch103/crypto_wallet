7e4123caf234c27a50041cd4cbb8f1e88df087ff
flinch103 <flinch103@gmail.com>
2019-02-11 15:54:18 +0530

    Setup devise for user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
+# Application Controller control throughout the application.
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name username role])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name username role])
+  end
 end
