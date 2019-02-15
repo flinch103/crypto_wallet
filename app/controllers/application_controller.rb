4e9cfccf61fcc45357fff2e82f928a1282be95c6
flinch103 <flinch103@gmail.com>
2019-02-15 16:37:14 +0530

    User login with username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,19 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name username role])
-    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name username role])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[full_name username role email])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[full_name username role email])
+  end
+
+  private
+
+  def after_sign_in_path_for(resource)
+    unless resource.role.eql?(params[:user][:role])
+      sign_out resource
+      flash.delete(:notice)
+      set_flash_message!(:error, :role_not_match)
+      return new_user_session_path
+    end
+    root_path
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,14 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
-  enum bet_type: { vodiant: 0, vodeer: 1, arbiter: 2 }
+         :recoverable, :rememberable, :validatable, :confirmable, authentication_keys: [:username]
 
-  validates :username, :email, uniqueness: true, presence: true
+  enum role: { vodiant: 0, vodeer: 1, arbiter: 2 }
+
+  validates :username, uniqueness: true, presence: true
   validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validate :validate_username
+  has_one :wallet, dependent: :destroy
 
   def validate_username
     errors.add(:username, :invalid) if User.where(email: username).exists?
