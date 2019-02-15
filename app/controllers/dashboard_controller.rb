d451509b5431bc1ef2c22fba7c83b0bd3cda3d79
flinch103 <flinch103@gmail.com>
2019-02-15 16:50:30 +0530

    Added setup account functionality

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,12 @@
 # Dashboard Controller
 class DashboardController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_wallet_setup
   def index; end
+
+  private
+
+  def check_wallet_setup
+    redirect_to setup_accounts_path(:policy) if current_user.wallet.blank?
+  end
 end
--- app/controllers/setup_accounts_controller.rb
+++ app/controllers/setup_accounts_controller.rb
@@ -1,6 +1,10 @@
+# SetupAccounts Controller
 class SetupAccountsController < ApplicationController
-	layout 'setup_accounts'
-	def index; end
+  layout 'setup_accounts'
+  def index
+    page_name = params[:page_name]
+    render page_name
+  end
 
-	def setup; end
+  def setup; end
 end
