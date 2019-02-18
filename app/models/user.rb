dcffd085ec75de8eb686672f1659e8bece4b81b1
flinch103 <flinch103@gmail.com>
2019-02-18 14:08:37 +0530

    task model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, authentication_keys: [:username]
 
-  enum role: { vodiant: 0, vodeer: 1, arbiter: 2 }
+  enum role: [:vodiant, :vodeer, :arbiter]
 
   validates :username, uniqueness: true, presence: true
   validates :username, format: { with: /\A[a-zA-Z0-9]+\Z/ }
   validate :validate_username
   has_one :wallet, dependent: :destroy
+
+  has_many :tasks, class_name: 'Task', foreign_key: 'vodiant_id'
+  has_many :assigned_tasks, class_name: 'Task', foreign_key: 'vodeer_id'
+  has_many :disputed_tasks, class_name: 'Task', foreign_key: 'arbiter_id'
 
   def validate_username
     errors.add(:username, :invalid) if User.where(email: username).exists?
