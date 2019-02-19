ff9a914202ad88d4bfd56f1ec7c6ccad72f2929b
flinch103 <flinch103@gmail.com>
2019-02-19 22:50:36 +0530

    Front end logic to task payment

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -3,5 +3,5 @@ class Transaction < ApplicationRecord
   belongs_to :wallet
   belongs_to :task, class_name: 'Task', optional: true
   enum status: { pending: 0, success: 1, rejected: 2 }
-  enum tx_type: { 'platform_stack': 0, 'wage': 1 }
+  enum tx_type: { 'platform_stack': 0, 'wage': 1, 'task_stack': 2 }
 end
