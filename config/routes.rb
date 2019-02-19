b34da6bcd15a7396eabd4b4e75cdbf5daa5af9e2
flinch103 <flinch103@gmail.com>
2019-02-19 16:35:09 +0530

    create new task page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   root to: 'dashboard#index'
   resources :restore_wallets
   resources :task_details
+  resources :create_new_task
 
   # Health check Endpoints
   get '/_liveness', to: 'health_checks#health'
