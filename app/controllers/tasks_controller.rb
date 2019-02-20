7af8c8182ea38cb393efc19afebe5e3bc3a968aa
flinch103 <flinch103@gmail.com>
2019-02-20 11:51:37 +0530

    WIP: task listing

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -7,6 +7,14 @@ class TasksController < ApplicationController
 
   def index
     @tasks = get_tasks
+    @task_type = "Open Microtasks" unless current_user.arbiter?
+    @task_type = "Assigned Microtasks" if current_user.arbiter?
+    if params[:filter_type].present?
+      @task_type = "#{params[:filter_type].humanize} Microtasks"
+      render partial: 'vodiant_list' if request.xhr? && current_user.vodiant?
+      render partial: 'vodeer_list' if request.xhr? && current_user.vodeer?
+      render partial: 'arbiter_list' if request.xhr? && current_user.arbiter?
+    end   
   end
 
   def create
@@ -37,9 +45,23 @@ class TasksController < ApplicationController
   end
 
   def get_tasks
-    return current_user.tasks if current_user.vodiant?
-    return current_user.assigned_tasks if current_user.vodeer?
-
-    current_user.disputed_tasks
+    filter  = params[:filter_type]
+    if current_user.vodiant?
+      return current_user.tasks.completed if filter == 'completed'
+      return current_user.tasks.progress if filter == 'ongoing'
+      return current_user.tasks.disputed if filter == 'disputed'
+      current_user.tasks.open
+    elsif current_user.vodeer?
+      return current_user.assigned_tasks.completed if filter == 'completed'
+      return current_user.assigned_tasks.progress if filter == 'ongoing'
+      return current_user.assigned_tasks.disputed if filter == 'disputed'
+      return current_user.assigned_tasks.closed if filter == 'approved'
+      return current_user.assigned_tasks.rejected if filter == 'rejected'
+      Task.open
+    else
+      return current_user.tasks.completed if filter == 'completed'
+      return current_user.tasks.progress if filter == 'ongoing'
+      current_user.disputed_tasks
+    end
   end
 end
