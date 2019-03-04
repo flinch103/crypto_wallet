0cbd7f2db05a8364378e0d0a7a4da73d579d0ef6
flinch103 <flinch103@gmail.com>
2019-03-04 17:36:04 +0530

    remove flash message

--- app/controllers/setup_accounts_controller.rb
+++ app/controllers/setup_accounts_controller.rb
@@ -1,6 +1,7 @@
 # SetupAccounts Controller
 class SetupAccountsController < ApplicationController
   before_action :authenticate_user!
+  before_action :check_wallet_setup
   layout 'setup_accounts'
   def index
     page_name = params[:page_name]
@@ -8,4 +9,13 @@ class SetupAccountsController < ApplicationController
   end
 
   def setup; end
+
+  private
+
+  def check_wallet_setup
+    unless current_user.arbiter?
+      return redirect_to root_path if current_user.platform_stack_tx.present?
+    end
+  end
+
 end
---
+++ app/controllers/users/sessions_controller.rb
@@ -0,0 +1,9 @@
+class Users::SessionsController < Devise::SessionsController
+
+  private
+
+  def after_sign_out_path_for(resource)
+    new_user_session_path
+  end
+
+end
