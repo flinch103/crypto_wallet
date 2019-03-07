8b269dc5f38a651b57370e9914d6eb812db57c42
flinch103 <flinch103@gmail.com>
2019-03-07 18:02:36 +0530

    fixed migration

--- db/migrate/20190306123706_rename_avatar.rb
+++ db/migrate/20190306123706_rename_avatar.rb
@@ -1,9 +1,5 @@
 class RenameAvatar < ActiveRecord::Migration[5.2]
   def change
-    remove_column :users, :avatar_file_name
-    remove_column :users, :avatar_content_type
-    remove_column :users, :avatar_file_size
-    remove_column :users, :avatar_updated_at
     add_column :users, :avatar, :string
   end
 end
