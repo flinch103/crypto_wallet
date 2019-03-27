c896720841337a5b2a7db34ea4d3083605fe43d5
flinch103 <flinch103@gmail.com>
2019-03-27 17:37:18 +0530

    add fields to create task form and modify task listing page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -55,7 +55,7 @@ class TasksController < ApplicationController
   private
 
   def task_params
-    params.require(:task).permit(:title, :description, :end_date, :wage, :status, :dispute_comment, :rejection_comment, :vodeer_id, :resolved_id)
+    params.require(:task).permit(:title, :description, :start_date, :end_date, :max_vodeer, :wage, :status, :dispute_comment, :rejection_comment, :vodeer_id, :resolved_id)
   end
 
   def response_message
