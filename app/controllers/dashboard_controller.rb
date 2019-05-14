d3a934f519b8bc2ca6cb267ef3a21ff4c03602ce
flinch103 <flinch103@gmail.com>
2019-05-14 14:15:18 +0530

    add pagination and display start date instead of created at

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,12 +2,13 @@
 class DashboardController < ApplicationController
   before_action :authenticate_user!
   before_action :check_wallet_setup
-  
+  require 'will_paginate/array'
+
   def index
-    @tasks = get_tasks
-    @disputed_tasks = current_user.tasks.disputed&.order('created_at DESC')
-    @recent_published_tasks = Task.open&.order('created_at DESC')
-    @recent_working_tasks = current_user.assigned_tasks.progress&.order('created_at DESC')
+    @tasks = get_tasks.paginate(page: params[:page], per_page: 10)
+    @disputed_tasks = current_user.tasks.disputed&.order('created_at DESC').paginate(page: params[:page], per_page: 10)
+    @recent_published_tasks = Task.open&.order('created_at DESC').select{|task| task.is_valid? }.paginate(page: params[:page], per_page: 10)
+    @recent_working_tasks = current_user.assigned_tasks.progress&.order('created_at DESC').select{|task| task.is_valid? }.paginate(page: params[:page], per_page: 10)
     @wallet = current_user.wallet
     @platform_stack_tx = current_user.platform_stack_tx unless current_user.arbiter?
   end
