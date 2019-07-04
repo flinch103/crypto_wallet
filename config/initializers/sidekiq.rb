daee1191a554fd779f111d0b38835122df6bfeaa
flinch103 <flinch103@gmail.com>
2019-07-04 11:30:51 +0530

    set id to nil in sidekiq config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: Figaro.env.REDIS_URL }
+  config.redis = { url: Figaro.env.REDIS_URL, id: nil }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: Figaro.env.REDIS_URL }
+  config.redis = { url: Figaro.env.REDIS_URL, id: nil }
 end
